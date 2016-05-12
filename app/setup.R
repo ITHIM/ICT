4bcb354c43a074692d1cd3187f2f5351b827295a
usr110 <m.aliabbas@gmail.com>
2016-05-12 11:46:33 +0100

    Comment all installation packages code

--- app/setup.R
+++ app/setup.R
@@ -12,12 +12,14 @@ pkgs <- c(
 # Which packages do we require?
 # lapply(pkgs, library, character.only = T)
 reqs <- as.numeric(lapply(pkgs, require, character.only = TRUE))
-# Install packages we require
-if(sum(!reqs) > 0) install.packages(pkgs[!reqs])
 
-if(!require(rCharts)) devtools::install_github("rCharts", "ramnathv")
-
-if(!require(shiny)) devtools::install_github("shiny", "rstudio")
+# Committing all the installation packages
+# # Install packages we require
+# if(sum(!reqs) > 0) install.packages(pkgs[!reqs])
+# 
+# if(!require(rCharts)) devtools::install_github("rCharts", "ramnathv")
+# 
+# if(!require(shiny)) devtools::install_github("shiny", "rstudio")
 
 source("data-processing.R")
 # Functions
