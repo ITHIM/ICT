63b396a256d1b0a8b6e89a0c5090c15f2b34fc5f
usr110 <m.aliabbas@gmail.com>
2016-12-21 15:08:28 +0000

    Reintroduce `total population` for car miles. Relevant to https://github.com/ITHIM/ICT/issues/41

--- app/ui.R
+++ app/ui.R
@@ -40,10 +40,10 @@ METSwitchRButton <- c("Baseline and Scenario" = "sep",
 switchRButton <- c("Scenario versus Baseline/alternative Scenario" =    "comp",
                    "Sub-population versus total population" = "sep")
 
-# denominatorRButton <- c("Total Population" = "pop",
-#                         "Total Cyclists" = "cyc")
+denominatorRButton <- c("Total Population" = "pop",
+                       "Total Cyclists" = "cyc")
 
-denominatorRButton <- c("Total Cyclists" = "cyc")
+# denominatorRButton <- c("Total Cyclists" = "cyc")
 
 
 phyGLRButton <- c("Marginal MET hours" =    "off",
